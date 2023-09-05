80271b49526837911db16f8ab91f617a6eca816f
demirezenmert <demirezenmert@gmail.com>
2023-09-05 00:06:13 -0400

    Logging message func done.

--- file_manager_automation.py
+++ file_manager_automation.py
@@ -7,13 +7,17 @@
 
 
 # Module
-
 import os
+from os import scandir, rename, getcwd
 from time import sleep
 from os.path import exists, join, splitext
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from shutil import move 
+from time import sleep
+import logging
+
+
 # you can use os.rename() but doesnt support different disk drives.
 # from os import rename
 
@@ -21,8 +25,13 @@ from shutil import move
 #Directories
 
 srcDirectory = '/Users/mertdemirezen/Documents/Projects/FileManager'
+picDir = 'Pictures'
+videoDir = 'Videos'
+musicDir = 'Musics'
+docDir = 'Documents'
+sfxDir = 'Sfx'
 
-
+source = [picDir,videoDir,musicDir,docDir,sfxDir]
 
 
 # supported image types
@@ -34,35 +43,114 @@ video_extensions = [".webm", ".mpg", ".mp2", ".mpeg", ".mpe", ".mpv", ".ogg",
 # supported Audio types
 audio_extensions = [".m4a", ".flac", "mp3", ".wav", ".wma", ".aac"]
 # supported Document types
-document_extensions = [".doc", ".docx", ".odt",".pdf", ".xls", ".xlsx", ".ppt", ".pptx"]
+document_extensions = [".doc", ".docx", ".odt",".pdf", ".xls", ".xlsx", ".ppt", ".pptx","txt"]
 
-def makeUnique(dest, fileName):
-    #seperating name and extention from file name
-    name, extention = splitext(fileName)
-    fileCounter = 1
-    #if file exists in destination folder, adds number to the end of the file name
-    while exists(f'{dest}/{name}'):
-        fileName = f'{name}({str(fileCounter)}){extention}'
-        fileCounter += 1
-    return fileName
-def moveFile(dest, entry, fileName):
-    if exists(f'{dest}/{fileName}'):
-        makeUnique(dest, fileName=fileName)
 
+
+
+ 
 
 
 class FileManager(FileSystemEventHandler):
 
+    def __init__(self):
+        self.isFoldersExist()
+
     def on_modified(self, event):
 
-        with os.scandir(srcDirectory) as entries :
+        with scandir(srcDirectory) as entries :
             for entry in entries:
                 fileName = entry.name
                 
                 #Checking is image?
-                self.checkImageFiles(entry, fileName)
+                self.checkImageFiles(fileName)
+                self.checkAudioFiles(entry,fileName)
+                self.checkVideoFiles(fileName)
+                self.checkDocumentFiles(entry,fileName)
 
-    def checkImageFiles(self, entry, fileName):
+
+
+    def checkAudioFiles(self, entry, name):  
+        for audio_extension in audio_extensions:
+            if name.endswith(audio_extension) or name.endswith(audio_extension.upper()):
+                if entry.stat().st_size < 10_000_000 or "SFX" in name:  # ? 10Megabytes
+                    dest = sfxDir
+                else:
+                    dest = musicDir
+                self.moveFile(dest,name)
+                sleep(0.2)
+                logging.info(f"Moved audio file: {name}")
+
+    def checkVideoFiles(self, name):  
+        for video_extension in video_extensions:
+            if name.endswith(video_extension) or name.endswith(video_extension.upper()):
+                self.moveFile(videoDir,name)
+                sleep(0.2)
+                logging.info(f"Moved video file: {name}")
+    
+
+    def checkImageFiles(self, fileName):
         for image_ext in image_extensions:
             if fileName.endswith(image_ext) or fileName.endswith(image_ext.upper()):
-                moveFile(destinationDirImage, entry, fileName)
+                self.moveFile(picDir, fileName)
+                logging.info(f"Moved image file: {fileName}")
+                sleep(0.2)
+    
+    
+    
+    def checkDocumentFiles(self, entry, name):  # * Checks all Document Files
+        for documents_extension in document_extensions:
+            if name.endswith(documents_extension) or name.endswith(documents_extension.upper()):
+                self.moveFile(docDir,name)
+                logging.info(f"Moved document file: {name}")
+                sleep(0.2)
+
+
+
+
+    def isFoldersExist(self):
+        isPicDir = os.path.isdir(picDir)
+        for dir in source:
+            if not os.path.isdir(dir):
+                os.mkdir(dir)
+       
+    
+    
+    def moveFile(self, dest, fileName):
+        
+        if exists(f'{dest}/{fileName}'):
+            uniqueFileName = self.makeUnique(dest, fileName=fileName)
+            rename(fileName, uniqueFileName)
+
+        if not exists(f'{dest}/{fileName}') :
+            move(fileName, dest)
+
+    def makeUnique(self, dest, fileName):
+        #seperating name and extention from file name
+        name, extention = splitext(fileName)
+        fileCounter = 1
+        
+        #if file exists in destination folder, adds number to the end of the file name
+        while exists(f'{dest}/{fileName}'):
+            
+            fileName = f'{name}({str(fileCounter)}){extention}'
+            fileCounter += 1
+        return fileName
+
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = getcwd()
+    event_handler = FileManager()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            sleep(10)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
